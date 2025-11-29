e33e852b7535d9635a37cada038fdc07f3830c8c
lonebono <vaughnsy15@gmail.com>
2025-11-30 02:41:54 +0800

    some setup

--- docs/manual_setup.sh
+++ docs/manual_setup.sh
@@ -15,6 +15,11 @@ mysql -e "CREATE DATABASE IF NOT EXISTS imdbDDB;"
 mysql -e "CREATE USER IF NOT EXISTS 'mco2'@'%' IDENTIFIED BY 'stadvdbgroup10';"
 mysql -e "GRANT ALL PRIVILEGES ON imdbDDB.* TO 'mco2'@'%'; FLUSH PRIVILEGES;"
 
+# install git
+apt install -y git #install git to node 1 and 2
+
+# did not use below yet
+
 # install curl
 apt install -y curl
 
@@ -24,10 +29,6 @@ apt install -y nodejs
 
 # install pm2 (process manager)
 npm install -g pm2
-
-# install git
-apt install -y git
-
 
 # set pm2 to start on boot
 pm2 start server.js --name webapp
